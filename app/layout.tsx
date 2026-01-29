a884d09184e8fa8f64b68caf438a26ce8d6c83a8
wufulin <wufulinit@gmail.com>
2026-01-30 00:56:17 +0800

    fix: remove Google Fonts dependency for offline build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 'use client'
 
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Provider } from 'react-redux'
 import { store } from './store'
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function RootLayout({
   children,
@@ -22,9 +11,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-CN">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <Provider store={store}>{children}</Provider>
       </body>
     </html>
