1eef11bb2d931a4210613578c53d65b5c9f6f9ad
wufulin <wufulinit@gmail.com>
2026-01-30 00:38:56 +0800

    feat: add gallery slice with localStorage persistence

--- app/store/index.ts
+++ app/store/index.ts
@@ -3,13 +3,15 @@ import viewReducer from './slices/viewSlice'
 import imageReducer from './slices/imageSlice'
 import paramsReducer from './slices/paramsSlice'
 import frameReducer from './slices/frameSlice'
+import galleryReducer from './slices/gallerySlice'
 
 export const store = configureStore({
   reducer: {
     view: viewReducer,
     image: imageReducer,
     params: paramsReducer,
-    frame: frameReducer
+    frame: frameReducer,
+    gallery: galleryReducer
   }
 })
 
